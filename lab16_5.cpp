ba42f4bc59ef665eca3d0d3f1b13970452f2fc1c
AutsadaWiriya <aut0935261784@gmail.com>
2023-02-07 23:18:46 +0700

    Add

--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,7 +11,7 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
@@ -19,3 +19,24 @@ int main(){
 }
 
 //Write definition of shuffle() using pointer here 
+void shuffle(int *a, int *b, int *c, int *d){
+	int num[] = {*a,*b,*c,*d};
+	int size = sizeof(num)/sizeof(num[0]);
+	int num_new[size] = {};
+
+	num_new[0] = num[rand()%4] ;
+	do{
+		num_new[1] = num[rand()%4] ;
+	}while(num_new[1] == num_new[0])  ;
+	do{
+		num_new[2] = num[rand()%4] ;
+	}while(num_new[2] == num_new[0] or num_new[2] == num_new[1])  ;
+	do{
+		num_new[3] = num[rand()%4] ;
+	}while(num_new[3] == num_new[0] or num_new[3] == num_new[1] or num_new[3] == num_new[2])  ;
+
+	*a = num_new[0] ;
+	*b = num_new[1] ;
+	*c = num_new[2] ;
+	*d = num_new[3] ;
+}
